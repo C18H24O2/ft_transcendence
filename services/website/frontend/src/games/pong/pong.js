f9d1dc18ff8e6a35fbe35f5a6546fa038b84c9dd
Liam Vincent <lixam.vincent@gmail.com>
2024-08-25 00:17:20 +0200

    a start on some actual rendering code

--- services/website/frontend/src/games/pong/pong.js
+++ services/website/frontend/src/games/pong/pong.js
@@ -1,6 +1,7 @@
 import '../../shared.js';
 import { getTheme } from '../../theme.js';
 import { flavors } from '@catppuccin/palette';
+import earcut from 'earcut';
 
 var canvas = document.getElementById('gameField')
 /** @type {CanvasRenderingContext2D} */
@@ -11,20 +12,58 @@ var middleX = canvas.width / 2;
 var middleY = canvas.height / 2;
 var paddleHeight = canvas.height / 5;
 var paddleWidth = paddleHeight / 9;
-var ballSize = canvas.height / 50 * 1.5;
+var ballSize = canvas.height / 50 * 1.25;
 var ballDisplace = ballSize / 2;
 
 document.addEventListener('DOMContentLoaded', init);
 
-//Going for an oop approach, not too sure of how it works in js yet
+//Going for an oop approach, not too sure of how it works in js yet 
+
+function removeDuplicatePoints(a) {
+    var seen = {};
+    return a.filter(function(item) {
+        return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+    });
+}
+
+class Polygon {
+	constructor(verticeList) {
+		removeDuplicatePoints(verticeList);
+		this.verticeList = verticeList;
+	}
+	getVertices() { return this.verticeList}
+	render() {
+		ctx.beginPath();
+		ctx.moveTo(this.verticeList[0][0], this.verticeList[0][1]);
+		for (var i = 1; i < this.verticeList.length; i++) {
+			ctx.lineTo(this.verticeList[i][0], this.verticeList[i][1]);
+		}
+		ctx.fill();
+		ctx.closePath();
+		ctx.stroke();
+
+	}
+}
+
+//some default shapes to make my life easier (maybe)
+
+class ShapeMaker {
+	static makeTriangle(pointA, pointB, pointC) {}
+	static makeRectangle(x, y ,width, height) {
+		return new Polygon([[x, y],[x + width, y],[x + width, y + height],[x, y + height]]);
+	}
+	static makePolygon(x, y, radius, vertNum) {}
+	static makePolygon(vertList) {}
+}
 
 class Ball { 
 	constructor(x, y) {
 		this.x = x;
 		this.y = y;
+		this.shape = ShapeMaker.makeRectangle(this.x - ballDisplace, this.y - ballDisplace, ballSize, ballSize);
 	}
 	draw() {
-		ctx.fillRect(this.x - ballDisplace, this.y - ballDisplace , ballSize, ballSize);
+		this.shape.render();
 	}
 }
 
@@ -32,11 +71,47 @@ class Player {
 	constructor(x, y) {
 		this.x = x;
 		this.y = y;
+		this.shape = ShapeMaker.makeRectangle(this.x - paddleWidth / 2, this.y - paddleHeight / 2, paddleWidth, paddleHeight);
 	}
 	draw() {
-		ctx.fillRect(this.x - paddleWidth / 2, this.y - paddleHeight / 2, paddleWidth, paddleHeight);
+		this.shape.render();
 	}
 
+}
+
+//start for what should be an SAT collision detection system
+
+function boundingBox(vertList)
+{
+	var minX = Infinity, minY = Infinity;
+	var maxX = -Infinity, maxY = -Infinity;
+
+	vertList.forEach(([x, y]) => {
+		if (x < minX) minX = x;
+		if (x > maxX) maxX = x;
+		if (y < minY) minY = y;
+		if (y > maxY) maxY = y;
+	});
+	return { minX, minY, maxX, maxY };
+}
+
+function BoundingBoxCollide(bBoxA, bBoxB)
+{
+	var A_Left_B = bBoxA.maxX < bBoxB.minX;
+	var A_Right_B = bBoxA.minX > bBox.maxX;
+	var A_Above_B = bBoxA.maxY < bBoxB.minY;
+	var A_Below_B = bBoxA.minY > bBoxB.maxY;
+	return !(A_Left_B || A_Right_B || A_Above_B || A_Below_B);
+}
+
+function checkCollision(PolygonA, PolygonB)
+{
+
+	if (!boundingBoxCollide(boundingBox(PolygonA), boundingBox(PolygonB)))
+		return false;
+	var trianglesA = earcut(PolygonA.getVertices);
+
+	
 }
 
 //this makes it easier to scale more than 2 players if we do it
@@ -45,6 +120,7 @@ function renderField2d(ballList, playerList)
 {
 	var colors = flavors[getTheme().split('-').pop()].colors;
 	ctx.fillStyle = colors.surface2.hex;
+	ctx.strokeStyle = colors.surface2.hex;
 	
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ballList.forEach(ball => {
