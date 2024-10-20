e97935ab4cd77ba6bf9b342cb8e4da18f16d5378
Liam VINCENT <lixam.vincent@gmail.com>
2024-10-20 21:53:54 +0200

    changes to render logic to add more to the Shape object, and generally remove as much as possible from draw scene

--- services/website/frontend/src/games/3dpong/3dpong.js
+++ services/website/frontend/src/games/3dpong/3dpong.js
@@ -5,8 +5,13 @@ import { initShaders } from './webgl-initshader.js';
 import './webgl-shape.js';
 import { Shape2d } from './webgl-shape.js';
 import { Shape3d } from './webgl-shape.js';
-
 import { mat4 } from 'gl-matrix';
+
+/**@type {HTMLCanvasElement} */
+const canvas = document.getElementById("gameField");
+
+/**@type {WebGL2RenderingContext} */
+const gl = canvas.getContext("webgl2", {alpha: true});
 
 main();
 
@@ -28,12 +33,6 @@ function rgb_to_webgl(color)
 
 function main()
 {
-	/**@type {HTMLCanvasElement} */
-	const canvas = document.getElementById("gameField");
-
-	/**@type {WebGL2RenderingContext} */
-	const gl = canvas.getContext("webgl2", {alpha: true});
-
 	if (!gl)
 	{
 		console.error('Your browser does not support webgl, consider using a different browser to access this functionnality');
@@ -99,6 +98,12 @@ function main()
 		20, 22, 23, // left
 	];
 
+	const projectionMatrix = mat4.create();
+	const modelViewMatrix = mat4.create();
+
+	const projectionMatrix2 = mat4.create();
+	const modelViewMatrix2 = mat4.create();
+
 	var cube = new Shape3d(gl, programInfo, [
 		// Front face
 		-1.0, -1.0, 1.0,
@@ -135,65 +140,97 @@ function main()
 		-1.0, -1.0, 1.0,
 		-1.0, 1.0, 1.0,
 		-1.0, 1.0, -1.0,
-		], colors, indices);
+		], colors, indices, projectionMatrix, modelViewMatrix);
 
-		let then = Date.now();
+		var cube2 = new Shape3d(gl, programInfo, [
+			// Front face
+			-1.0, -1.0, 1.0,
+			1.0, -1.0, 1.0,
+			1.0, 1.0, 1.0,
+			-1.0, 1.0, 1.0,
+		  
+			// Back face
+			-1.0, -1.0, -1.0,
+			-1.0, 1.0, -1.0,
+			1.0, 1.0, -1.0,
+			1.0, -1.0, -1.0,
+		  
+			// Top face
+			-1.0, 1.0, -1.0,
+			-1.0, 1.0, 1.0,
+			1.0, 1.0, 1.0,
+			1.0, 1.0, -1.0,
+		  
+			// Bottom face
+			-1.0, -1.0, -1.0,
+			1.0, -1.0, -1.0,
+			1.0, -1.0, 1.0,
+			-1.0, -1.0, 1.0,
+		  
+			// Right face
+			1.0, -1.0, -1.0,
+			1.0, 1.0, -1.0,
+			1.0, 1.0, 1.0,
+			1.0, -1.0, 1.0,
+		  
+			// Left face
+			-1.0, -1.0, -1.0,
+			-1.0, -1.0, 1.0,
+			-1.0, 1.0, 1.0,
+			-1.0, 1.0, -1.0,
+			], colors, indices, projectionMatrix2, modelViewMatrix2);
+		
+		let then = 0;
 		let deltaTime = 0;
 		let cubeRotation = 0.0;
 
-		function render() {
-			let now = Date.now();
+		function render(timestamp) {
+			let now = timestamp * 0.001;
     		deltaTime = now - then;
 			then = now;
-
-			drawScene(gl, cube, cubeRotation);
-			cubeRotation += deltaTime / 1000;
+			drawScene(cube, cube2, cubeRotation);
+			cubeRotation += deltaTime;
 			requestAnimationFrame(render);
 		}
 		requestAnimationFrame(render);
 }
 
+//constants for the fov
 
-function drawScene(gl, object, cubeRotation)
+const fieldOfView = (45 * Math.PI) / 180;
+const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+const zNear = 0.1;
+const zFar = 100.0;
+
+function clearScene(gl_to_clear)
 {
-	const fieldOfView = (45 * Math.PI) / 180; // in radians
-	const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-	const zNear = 0.1;
-	const zFar = 100.0;
-	const projectionMatrix = mat4.create();
-	  
-	// note: glmatrix.js always has the first argument
-	// as the destination to receive the result.
-	mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
-	
-	const modelViewMatrix = mat4.create();
+	gl_to_clear.clearDepth(1.0);
+	gl_to_clear.enable(gl_to_clear.DEPTH_TEST);
+	gl_to_clear.depthFunc(gl_to_clear.LEQUAL);
+	gl_to_clear.clear(gl_to_clear.COLOR_BUFFER_BIT | gl_to_clear.DEPTH_BUFFER_BIT);
+}
 
-	mat4.translate(
-		modelViewMatrix, // destination matrix
-		modelViewMatrix, // matrix to translate
-		[-0.0, Math.sin(cubeRotation), -6.0]
-	); // amount to translate
-	
-	mat4.rotate(
-		modelViewMatrix, // destination matrix
-		modelViewMatrix, // matrix to rotate
-		cubeRotation, // amount to rotate in radians
-		[0, 0, 1]
-	); // axis to rotate around (Z)
-	mat4.rotate(
-		modelViewMatrix, // destination matrix
-		modelViewMatrix, // matrix to rotate
-		cubeRotation * 0.7, // amount to rotate in radians
-		[0, 1, 0]
-	); // axis to rotate around (Y)
-	mat4.rotate(
-		modelViewMatrix, // destination matrix
-		modelViewMatrix, // matrix to rotate
-		cubeRotation * 0.3, // amount to rotate in radians
-		[1, 0, 0]
-	);
+//example of a scene draw
+function drawScene(object, object2, cubeRotation)
+{	
+	clearScene(gl);
+	mat4.identity(object.modelViewMatrix);
+	mat4.identity(object.projectionMatrix);
 
-	const mtpMatrix = mat4.create();
-	mat4.multiply(mtpMatrix, projectionMatrix, modelViewMatrix);
-	object.draw(mtpMatrix);
+	mat4.identity(object2.modelViewMatrix);
+	mat4.identity(object2.projectionMatrix);
+
+	mat4.perspective(object.projectionMatrix, fieldOfView, aspect, zNear, zFar);
+	object.translate([-1.0, 1.0, -6.0]);
+	object.rotate(cubeRotation, [0, 0, 1]);
+	object.rotate(cubeRotation * 0.7, [0, 1, 0]);
+	object.rotate(cubeRotation * 0.3, [1, 0, 0]);
+
+	mat4.perspective(object2.projectionMatrix, fieldOfView, aspect, zNear, zFar);
+	object2.translate([1.0, -1.0, -6.0]);
+	object2.rotate(-cubeRotation, [0, 0, 1]);
+	object2.rotate(-cubeRotation * 0.7, [0, 1, 0]);
+	object2.rotate(-cubeRotation * 0.3, [1, 0, 0]);
+	object.draw();
+	object2.draw();
 }
--- services/website/frontend/src/games/3dpong/webgl-shape.js
+++ services/website/frontend/src/games/3dpong/webgl-shape.js
@@ -12,32 +12,22 @@ class Shape2d
 	 * @param {Array} vertice 
 	 * @param {Array} color 
 	 */
-	constructor(gl, programInfo, vertice, color)
+	constructor(gl, programInfo, vertice, color, projectionMatrix, modelViewMatrix)
 	{
 		/** @type {WebGL2RenderingContext} */
 		this.gl = gl;
 		this.programInfo = programInfo;
 		this.vertexCount = vertice.length;
 		this.buffers = initBuffers(gl, vertice, color);
+		this.projectionMatrix = projectionMatrix || mat4.create();
+		this.modelViewMatrix = modelViewMatrix || mat4.create();
 	}
 	/**
 	 * 
 	 * @param {Array | mat4} matricesList 
 	 */
-	draw(matricesList)
+	draw(matrix)
 	{
-		this.gl.clearDepth(1.0); // Clear everything
-		this.gl.enable(this.gl.DEPTH_TEST); // Enable depth testing
-		this.gl.depthFunc(this.gl.LEQUAL); // Near things obscure far things
-
-		this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
-
-		let mvpMatrix = mat4.create();
-
-		matricesList.forEach(matrix =>{
-			mat4.multiply(mvpMatrix, mvpMatrix, matrix);
-		});
-
 
 		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffers.position);
 		this.gl.vertexAttribPointer(this.programInfo.attribLocation.vertexPosition, 2, this.gl.FLOAT, false, 0, 0);
@@ -48,18 +38,29 @@ class Shape2d
 		this.gl.enableVertexAttribArray(this.programInfo.attribLocation.vertexColor);
 
 		this.gl.useProgram(this.programInfo.program);
-
-		this.gl.uniformMatrix4fv(this.programInfo.uniformLocation.mtpMatrix, false, mvpMatrix);
+		if (matrix !== undefined)
+			this.gl.uniformMatrix4fv(this.programInfo.uniformLocation.mtpMatrix, false, matrix);
+		else
+		{
+			matrix = mat4.create();
+			mat4.multiply(matrix, this.projectionMatrix, this.modelViewMatrix);
+			this.gl.uniformMatrix4fv(this.programInfo.uniformLocation.mtpMatrix, false, matrix);
+		}
 		this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, this.vertexCount / 2);
 	}
+	rotate(radians, axis)
+	{
+		mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, radians, axis);
+	}
+	translate(amount)
+	{
+		mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, amount);
+	}
+	scale(vector)
+	{
+		mat4.scale(this.modelViewMatrix, this,this.modelViewMatrix, vector);
+	}
 }
-
-const checkGLError = (gl) => {
-    const error = gl.getError();
-    if (error !== gl.NO_ERROR) {
-        console.error('WebGL Error:', error);
-    }
-};
 
 class Shape3d
 {
@@ -70,13 +71,15 @@ class Shape3d
 	 * @param {Array} vertice 
 	 * @param {Array} color 
 	 */
-	constructor(gl, programInfo, vertice, color, indices)
+	constructor(gl, programInfo, vertice, color, indices, projectionMatrix, modelViewMatrix)
 	{
 		/** @type {WebGL2RenderingContext} */
 		this.gl = gl;
 		this.programInfo = programInfo;
 		this.vertexCount = indices.length;
 		this.buffers = initBuffers3d(gl, vertice, color, indices);
+		this.projectionMatrix = projectionMatrix || mat4.create();
+		this.modelViewMatrix = modelViewMatrix || mat4.create();
 	}
 	/**
 	 * 
@@ -84,12 +87,6 @@ class Shape3d
 	 */
 	draw(matrix)
 	{
-		this.gl.clearDepth(1.0); // Clear everything
-		this.gl.enable(this.gl.DEPTH_TEST); // Enable depth testing
-		this.gl.depthFunc(this.gl.LEQUAL); // Near things obscure far things
-
-		this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
-
 		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffers.position);
 		this.gl.vertexAttribPointer(this.programInfo.attribLocation.vertexPosition, 3, this.gl.FLOAT, false, 0, 0);
 		this.gl.enableVertexAttribArray(this.programInfo.attribLocation.vertexPosition);
@@ -100,8 +97,29 @@ class Shape3d
 
 		this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.buffers.indices);
 		this.gl.useProgram(this.programInfo.program);
-		this.gl.uniformMatrix4fv(this.programInfo.uniformLocation.mtpMatrix, false, matrix);
+		if (matrix !== undefined)
+		{
+			this.gl.uniformMatrix4fv(this.programInfo.uniformLocation.mtpMatrix, false, matrix);
+		}
+		else
+		{
+			matrix = mat4.create();
+			mat4.multiply(matrix, this.projectionMatrix, this.modelViewMatrix);
+			this.gl.uniformMatrix4fv(this.programInfo.uniformLocation.mtpMatrix, false, matrix);
+		}
 		this.gl.drawElements(this.gl.TRIANGLES, this.vertexCount, this.gl.UNSIGNED_SHORT, 0);
+	}
+	rotate(radians, axis)
+	{
+		mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, radians, axis);
+	}
+	translate(amount)
+	{
+		mat4.translate(this.modelViewMatrix, this.modelViewMatrix, amount);
+	}
+	scale(vector)
+	{
+		mat4.scale(this.modelViewMatrix, this,this.modelViewMatrix, vector);
 	}
 }
 
