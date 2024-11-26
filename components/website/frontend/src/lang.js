fa42c55981ac15ecb14c10f9b0e7c207e749fde2
Liam Vincent <lixam.vincent@gmail.com>
2024-11-26 03:35:16 +0100

    adding an update cookie function, I am tired and stupid

--- components/website/frontend/src/lang.js
+++ components/website/frontend/src/lang.js
@@ -1,34 +1,57 @@
 export function change_lang(value)
 {
-    console.log(value);
-    var current = getCookie('ft-lang');
+	console.log(value);
+	var current = getCookie('ft-lang');
 
-    if (value !== current)
-    {
-        setCookie('ft-lang', value);
-        location.reload();
-    }
+	if (value !== current)
+	{
+		updateCookie('ft-lang', value);
+		location.reload();
+	}
 }
+
+function initCookie()
+{
+	window.cookieMap = document.cookie.split(';').map('=');
+}	
 
 window.change_lang = change_lang;
 
 function setCookie(name,value,days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+	var expires = "";
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + (days*24*60*60*1000));
+		expires = "; expires=" + date.toUTCString();
+	}
+	document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function updateCookie(name, value, days)
+{
+	var cookieMap = document.cookie.split(';').map(v => v.split("="));
+	var expires = "";
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + (days*24*60*60*1000));
+		expires = "; expires=" + date.toUTCString();
+	}
+	var cookieString;
+	cookieMap[name] = (value || "") + expires + "; path=/";
+	window.cookie = "";
+	for (var key in cookieMap)
+	{
+		console.log(key);
+	}
 }
 
 function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
 }
