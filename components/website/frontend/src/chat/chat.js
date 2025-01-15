562b95901bb0c641870e092de5e02229fadccc78
xtrm <oss@xtrm.me>
2025-01-15 18:37:24 +0100

    🔨 chore: rework and cleanup networks, refactor api-service into microservices
    Signed-off-by: xtrm <oss@xtrm.me>

--- components/website/frontend/src/chat/chat.js
+++ components/website/frontend/src/chat/chat.js
@@ -4,11 +4,11 @@ const invite = document.querySelector('#game-invite-btn');
 const profile = document.querySelector('#profile-btn');
 const friendList = document.querySelector('#users');
 const chatSocket = new WebSocket(
-//    'wss://'
-//    + window.location.host
-//    + '/ws/v1/chat/'
-	//
-	'ws://localhost:18942/chat'
+   'wss://'
+   + window.location.host
+   + '/ws/v1/chat/'
+
+	// 'ws://localhost:18942/chat'
 );
 
 let selectedFriend = null; // Current friend being chatted with
