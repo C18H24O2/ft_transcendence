cde66a6362a4c5bd6f498444ac9fbfdbe05652a1
Liam Vincent <lixam.vincent@gmail.com>
2025-01-29 23:16:52 +0100

    uuuuh auth service or something like taht

--- components/services/microservices/auth-service/auth-service.py
+++ components/services/microservices/auth-service/auth-service.py
@@ -2,6 +2,8 @@ from service_runtime.service import message
 from service_runtime.models import User
 from controller import validate_jwt
 from argon2 import PasswordHasher
+import peewee
+import re
 
 hasher = PasswordHasher(time_cost=10)
 
@@ -18,15 +20,22 @@ def get_user(token: str) -> User:
 
 @message
 def register(username: str, password: str, totp_secret: str, totp_code: str) -> dict:
-    # TODO: validate username, password, token&code (size, format, etc)
+	# TODO: validate username, password, token&code (size, format, etc)
+	pattern = r"^[a-zA-Z0-9_-]{3,32}$"
+
+	if not bool(re.fullmatch(pattern, username)):
+		return  #Username is invalid TODO: valid error code
+
     # TODO: check if user exists
-
+	if User.select().where(User.username == username).exists():
+		return #User already exists TODO: valid error code
+      
     # TODO: hash password w/ argon2
-    pwhash = hasher.hash(username + password)
-    # TODO: create user in database
+	pwhash = hasher.hash(username + password)
+	# TODO: create user in database
 
-    # TODO: generate JWT with user
-    return {"token": "fuck you"}
+	# TODO: generate JWT with user
+	return {"token": "fuck you"}
 
 
 @message
