58671f7087e3ff77ba4b7d26d94726251dfef6d5
Liam VINCENT <lixam.vincent@gmail.com>
2025-01-30 05:44:04 +0100

    work start on 2fa

--- components/website/frontend/src/register/register.js
+++ components/website/frontend/src/register/register.js
@@ -1,1 +1,23 @@
-import '../shared.js'
+import '../shared.js';
+import { authenticator } from 'otplib';
+
+const issuer = "ft_trans";
+let secret;
+
+function generateQR(username)
+{
+	const otpAuthUrl = authenticator.keyuri(username, issuer, secret);
+	//TODO: generate qrcode
+}
+
+function ctor()
+{
+	secret = authenticator.generateSecret(20);
+}
+
+function dtor()
+{
+
+}
+
+setupPage(ctor, dtor)
